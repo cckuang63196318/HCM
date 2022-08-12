f8d31e74f5b77eba9784a051e7f45706c047750f
cck <cckuang6319@gmail.com>
2022-08-12 21:55:38 +0800

    vvv

---
+++ serviceWorker.js
@@ -0,0 +1,55 @@
+const cacheVersion = "v1.47";
+const filesToCache = [
+  '/HCM/static/js/',
+  '/HCM/static/img/',
+  '/HCM/static/fonta/',
+  '/HCM/static/css/',
+  'index.html',
+  'manifest.json',
+  'serviceWorker.js',
+  'innolux-offical.png',
+  'personIcon.png',
+  'working.png',
+  'favicon.ico',
+  'favicon72.png',
+  'favicon96.png',
+  'favicon144.png',
+  'favicon192.png',
+  'favicon512.png'
+];
+
+self.addEventListener("install", event => {
+  console.log('[ServiceWorker] Install');
+  event.waitUntil(
+    caches.open(cacheVersion).then(cache => {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    }),
+  );
+});
+
+self.addEventListener('activate', event => {
+  console.log('[ServiceWorker] Activate');
+  event.waitUntil(
+    caches.keys()
+    .then(keyList => {
+      return Promise.all(keyList.map(key => {
+        if (key !== cacheVersion) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {  
+  event.respondWith(
+    caches
+      .open(cacheVersion)
+      .then(cache => cache.match(event.request, { ignoreSearch: true }))
+      .then(response => {
+        console.log('[ServiceWorker] fetch', event.request);
+        return response || fetch(event.request);
+      }),
+  );
+});
